ff2e64f51705ab1b97fae0fef4bcfb0b93654c30
Abdelmalek Bensidhome <abdelmalek.bensidhoum@hotmail.com>
2026-01-04 17:31:06 +0100

    Add project files.

---
+++ BankProject/BankProject.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+#include "MyLib.h";
+using namespace std;
+
+
+
+
+
+int main()
+{
+
+	
+
+
+
+	system("pause>0");
+	return 0;
+}
+
